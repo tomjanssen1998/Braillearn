7d62867550848d41d3f6a978815cc500e3a73ac3
TimDriessen <45215498+TimDriessen@users.noreply.github.com>
2020-03-10 14:59:21 +0100

    Restructuring
    Code has better structure to give a better overview

--- __main__.py
+++ __main__.py
@@ -6,69 +6,17 @@ import time
 import Constant as c
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM) #referring to the pins by the "Broadcom SOC channel" numbers
+global engine # Engine used for TTS
 
-# Input braille cell
-GPIO.setup(c.BRAILLE_P1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 1
-GPIO.setup(c.BRAILLE_P2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 2
-GPIO.setup(c.BRAILLE_P3_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 3
-GPIO.setup(c.BRAILLE_P4_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 4
-GPIO.setup(c.BRAILLE_P5_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 5
-GPIO.setup(c.BRAILLE_P6_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 6
 
-# Control buttons
-GPIO.setup(c.NEXT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Next button
-GPIO.setup(c.RESET_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Reset button
-GPIO.setup(c.INDICATOR_UP, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Level indicator up
-GPIO.setup(c.INDICATOR_DOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Level indicator down
-
-# Setup callbacks
-GPIO.add_event_detect(c.BRAILLE_P1_PIN, GPIO.RISING, callback=g.callback0, bouncetime=c.BOUNCE_TIME)
-GPIO.add_event_detect(c.BRAILLE_P2_PIN, GPIO.RISING, callback=g.callback1, bouncetime=c.BOUNCE_TIME)
-GPIO.add_event_detect(c.BRAILLE_P3_PIN, GPIO.RISING, callback=g.callback2, bouncetime=c.BOUNCE_TIME)
-GPIO.add_event_detect(c.BRAILLE_P4_PIN, GPIO.RISING, callback=g.callback3, bouncetime=c.BOUNCE_TIME)
-GPIO.add_event_detect(c.BRAILLE_P5_PIN, GPIO.RISING, callback=g.callback4, bouncetime=c.BOUNCE_TIME)
-GPIO.add_event_detect(c.BRAILLE_P6_PIN, GPIO.RISING, callback=g.callback5, bouncetime=c.BOUNCE_TIME)
-
-GPIO.add_event_detect(c.NEXT_PIN, GPIO.RISING, callback=g.callbackNXT, bouncetime=c.BOUNCE_TIME)
-GPIO.add_event_detect(c.RESET_PIN, GPIO.RISING, callback=g.callbackRST, bouncetime=c.BOUNCE_TIME)
-GPIO.add_event_detect(c.INDICATOR_UP, GPIO.RISING, callback=g.callbackLVLUP, bouncetime=c.BOUNCE_TIME)
-GPIO.add_event_detect(c.INDICATOR_DOWN, GPIO.RISING, callback=g.callbackLVLDOWN, bouncetime=c.BOUNCE_TIME)
-
-"""
-engine = pyttsx3.init()
-engine.setProperty('rate', 140)
-engine.setProperty('voice', voices[49].id) #nederlands
-engine.say("Hallo, dit is een test voor ons programma.")
-engine.say("Dit is hoe deze library nederlands doet spreken")
-engine.say("Ik weet niet zeker of dit goed genoeg is")
-engine.runAndWait()
-engine.stop()
-"""
-
-def Read_text(text):
-    engine.say(text)
-    engine.runAndWait()
-    return
-
-def Prepare_TTS(text):
-    p = multiprocessing.Process(target = Read_text, args=(text,))
-    p.start()
-
-# w a i t F o r B r e a k ()
+# m a i n ()
 # ===========================
-# Only leaves loop when next-button or reset-button input occurs
+# Main function
 #
-def waitForBreak():
-    while True :
-        time.sleep(0.01) #Precautionary: Don't know if this is needed, but thought is that this delay gives the interrupt time to change variable
-        if (g.pressedNXT or g.pressedRST):
-            break
-
-#check whether this file is the file that is executed first
-if __name__ == '__main__':
-
-
+def main():
+    initializeGPIO()
+    initializeTTS()
+    
     #Dynamically load tasks when switching levels
     tasks = importlib.import_module("Tasks.Task1")
 
@@ -92,3 +40,91 @@ if __name__ == '__main__':
 
         if g.pressedRST:
                 g.pressedRST = False
+
+
+   
+
+# w a i t F o r B r e a k ()
+# ===============================
+# Only leaves loop when next-button or reset-button input occurs
+#
+def waitForBreak():
+    while True :
+        time.sleep(0.01) #Precautionary: Don't know if this is needed, but thought is that this delay gives the interrupt time to change variable
+        if (g.pressedNXT or g.pressedRST):
+            break
+
+# i n i t i a l i z e T T S ()
+# ===============================
+# initialize text-to-speech library
+#
+def initializeTTS():
+    global engine # So that we use the global 'engine' variable instead of creating a local one
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 140)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[49].id)
+
+    # === Voices ===
+    # 10 = default
+    # 11 = english
+    # 16 = english-us
+    # 49 = Nederlands
+
+# r e a d T e x t (text)
+# ===============================
+# uses text-to-speech to read out the text given as input
+#
+def readText(text):
+    engine.say(text) # Since we are not modifying 'engine' here but reading it, we don't need to put 'global engine' inside the method
+    engine.runAndWait()
+    return
+
+# p r e p a r e T T S (text)
+# ===============================
+# Creates a thread which runs the 'readText(text)' method with text given as input
+#
+def prepareTTS(text):
+    p = multiprocessing.Process(target = readText, args=(text,))
+    p.start()
+
+# i n i t i a l i z e G P I O ()
+# ===============================
+# initialize all GPIO pins used
+#
+def initializeGPIO():
+    GPIO.setmode(GPIO.BCM) #referring to the pins by the "Broadcom SOC channel" numbers
+
+    # Input braille cell
+    GPIO.setup(c.BRAILLE_P1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 1
+    GPIO.setup(c.BRAILLE_P2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 2
+    GPIO.setup(c.BRAILLE_P3_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 3
+    GPIO.setup(c.BRAILLE_P4_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 4
+    GPIO.setup(c.BRAILLE_P5_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 5
+    GPIO.setup(c.BRAILLE_P6_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Braille part 6
+
+    # Control buttons
+    GPIO.setup(c.NEXT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Next button
+    GPIO.setup(c.RESET_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Reset button
+    GPIO.setup(c.INDICATOR_UP, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Level indicator up
+    GPIO.setup(c.INDICATOR_DOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Level indicator down
+
+    # Setup callbacks
+    GPIO.add_event_detect(c.BRAILLE_P1_PIN, GPIO.RISING, callback=g.callback0, bouncetime=c.BOUNCE_TIME)
+    GPIO.add_event_detect(c.BRAILLE_P2_PIN, GPIO.RISING, callback=g.callback1, bouncetime=c.BOUNCE_TIME)
+    GPIO.add_event_detect(c.BRAILLE_P3_PIN, GPIO.RISING, callback=g.callback2, bouncetime=c.BOUNCE_TIME)
+    GPIO.add_event_detect(c.BRAILLE_P4_PIN, GPIO.RISING, callback=g.callback3, bouncetime=c.BOUNCE_TIME)
+    GPIO.add_event_detect(c.BRAILLE_P5_PIN, GPIO.RISING, callback=g.callback4, bouncetime=c.BOUNCE_TIME)
+    GPIO.add_event_detect(c.BRAILLE_P6_PIN, GPIO.RISING, callback=g.callback5, bouncetime=c.BOUNCE_TIME)
+
+    GPIO.add_event_detect(c.NEXT_PIN, GPIO.RISING, callback=g.callbackNXT, bouncetime=c.BOUNCE_TIME)
+    GPIO.add_event_detect(c.RESET_PIN, GPIO.RISING, callback=g.callbackRST, bouncetime=c.BOUNCE_TIME)
+    GPIO.add_event_detect(c.INDICATOR_UP, GPIO.RISING, callback=g.callbackLVLUP, bouncetime=c.BOUNCE_TIME)
+    GPIO.add_event_detect(c.INDICATOR_DOWN, GPIO.RISING, callback=g.callbackLVLDOWN, bouncetime=c.BOUNCE_TIME)    
+
+
+                
+# Check whether this file is the file that is executed first
+# This part should be at the bottom to ensure all called methods are defined before calling them
+if __name__ == '__main__':
+    main() 
