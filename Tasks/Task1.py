3532c992f0ced9859fa0b7434aa481001c7fe398
ivokersten <39556776+ivokersten@users.noreply.github.com>
2020-03-13 19:06:15 +0100

    Add files via upload

--- Tasks/Task1.py
+++ Tasks/Task1.py
@@ -1,1 +1,22 @@
-quiz = ['a', 'b', 'c']
+#Quiz options
+repeatUntilCorrect = True  # Repeats questions if answered incorrectly
+repeatImmediately = False  # Whether to repeat immediately or when all other questions are asked
+randomizeOrder = True      # Whether to ask questions in a random order or as they are given
+
+feedbackPerLetter = False   # Whether to provide feedback for every letter or for every question
+
+#Quiz content
+quiz = ['hoi', 'hallo', 'doei']
+
+
+
+
+
+# Sanity checks to ensure way of questioning makes sense
+# ========================================================
+
+# If quiz contains words, errors must be repeated immediately and until correct if feedback is provided per letter
+
+if len(max(quiz, key=len)) > 1 and feedbackPerLetter: 
+    repeatUntilCorrect = True
+    repeatImmediately = True
--- Tasks/Task2.py
+++ Tasks/Task2.py
@@ -1,1 +1,22 @@
-quiz = ['d', 'e', 'f']
+#Quiz options
+repeatUntilCorrect = True  # Repeats questions if answered incorrectly
+repeatImmediately = False  # Whether to repeat immediately or when all other questions are asked
+randomizeOrder = False     # Whether to ask questions in a random order or as they are given
+
+feedbackPerLetter = False  # Whether to provide feedback for every letter or for every question
+
+#Quiz content
+quiz = ['g', 'h', 'i']
+
+
+
+
+
+# Sanity checks to ensure way of questioning makes sense
+# ========================================================
+
+# If quiz contains words, errors must be repeated immediately and until correct if feedback is provided per letter
+
+if len(max(quiz, key=len)) > 1 and feedbackPerLetter: 
+	repeatUntilCorrect = True
+	repeatImmediately = True
